c08c03ef3e92aa5f3263e2a7f4a2c7fcbbf69f03
Hannah Krager <hannahkrager@gmail.com>
2015-07-07 14:33:57 -0700

    create first class

--- hw/hw17/html_render.py
+++ hw/hw17/html_render.py
@@ -8,4 +8,15 @@ Python class example.
 # The start of it all:
 # Fill it all in here.
 class Element(object):
-    pass
+
+    def __init__(self, name='', content=''):
+        self.name = name
+        self.content = content
+
+    def append(self, new_text):
+        self.content += new_text
+
+    def render(self, file):
+        file.write(self.content)
+
+
