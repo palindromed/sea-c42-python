7b227e054ac7ddd2f7354c48f31dda4f65c2915f
Hannah Krager <hannahkrager@gmail.com>
2015-07-08 16:23:01 -0700

    add body and p classes

--- hw/hw17/html_render.py
+++ hw/hw17/html_render.py
@@ -7,7 +7,12 @@ Python class example.
 
 # The start of it all:
 # Fill it all in here.
+
+
 class Element(object):
+    """ Renders HTML """
+    tag = 'html'
+    indent = ''
 
     def __init__(self, name='', content=''):
         self.name = name
@@ -16,7 +21,23 @@ class Element(object):
     def append(self, new_text):
         self.content += new_text
 
-    def render(self, file):
-        file.write(self.content)
+    def render(self, file, indent=''):
+        file.write('<' + self.tag + '>' + self.content + '</' + self.tag + '>')
 
 
+class Html(Element):
+    '''Create an HTML tag'''
+    tag = 'html'
+    indent = ''
+
+
+class Body(Element):
+    ''' Create a body tag '''
+    tag = 'body'
+    indent = '  '
+
+
+class P(Element):
+    ''' Create a p tag '''
+    tag = 'p'
+    indent = '      '
